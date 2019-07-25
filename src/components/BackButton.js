20458c43308a490f73310f8178ab93d538f8c42b
dexypro <bajovicdejan91@gmail.com>
2019-07-25 20:49:30 +0200

    adapting for netlify

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Back = ({ history }) => (
-   /*
+const Back = () => (
+  /*
       rubric43
       The user should see a button labeled “Back” 
    */
-   /*
+  /*
       rubric45
       Clicking the “Back” button should take the user back to where
       they came from, whether it was the Shopping page or the
       Product Page. 
 
    */
-   <button className="btn btn-lg btn-info" onClick={history.goBack}>Back</button>
+  <Link to="/">
+    <button className="btn btn-lg btn-info">
+      Back
+    </button>
+  </Link>
 );
 
-export default withRouter(Back);
+export default Back;
